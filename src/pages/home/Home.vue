c03e983a5669a245c8bbea54f23c329160b69279
comeon-heqiang <3097951776@qq.com>
2019-01-08 17:03:02 +0800

    使用keep-alive优化性能

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -14,15 +14,17 @@ import HomeSwiper from "./components/Swiper";
 import HomeIcons from "./components/Icons";
 import HomeRecommend from "./components/Recommend";
 import HomeWeekend from "./components/Weekend";
+import { mapMutations, mapState } from "vuex";
 import axios from "axios";
 export default {
   name: "Home",
   data() {
-    return {      
+    return {
       swiperData: [],
       iconsData: [],
       recommendData: [],
-      weekendData: []
+      weekendData: [],
+      lastCity: ""
     };
   },
   components: {
@@ -32,18 +34,40 @@ export default {
     HomeRecommend,
     HomeWeekend
   },
+  computed: {
+    ...mapState(["city"])
+  },
+  methods: {
+    initHomeData() {
+      axios
+        .get("/api/index.json", {
+          params: {
+            city: this.city
+          }
+        })
+        .then(response => {
+          let res = response.data;
+          if (res.ret && res.data) {
+            this.swiperData = res.data.swiperList;
+            this.iconsData = res.data.iconList;
+            this.weekendData = res.data.weekendList;
+            this.recommendData = res.data.recommendList;
+          } else {
+            console.log("获取首页数据失败");
+          }
+        });
+    }
+  },
   mounted() {
-    axios.get("/api/index.json").then(response => {
-      let res = response.data;
-      if (res.ret && res.data) {
-        this.swiperData = res.data.swiperList;
-        this.iconsData = res.data.iconList;
-        this.weekendData = res.data.weekendList;
-        this.recommendData=res.data.recommendList;
-      } else {
-        console.log("获取首页数据失败");
-      }
-    });
+    this.lastCity = this.city;
+    this.initHomeData();
+  },
+  //keepalive会使用缓存数据, 判断选择城市是否改变，如果改变则重新发送请求 ----activated声明周期钩子是keep-alive 组件激活时调用
+  activated() {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city;
+      this.initHomeData();
+    }
   }
 };
 </script>
