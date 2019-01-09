b7d9e3ae28b972438d90fd88c4481b425c544c26
comeon-heqiang <3097951776@qq.com>
2019-01-09 10:14:23 +0800

    详情页banner完成

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,79 @@
+<template>
+  <div
+    class="container"
+    @click="handleGallaryClick"
+  >
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+        <swiper-slide
+          v-for="(item,index) in imgs"
+          :key="index"
+        >
+          <img
+            class="gallary-img"
+            :src="item"
+          />
+        </swiper-slide>
+        <div
+          class="swiper-pagination"
+          slot="pagination"
+        ></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgs: {
+      type: Array,
+      default() {
+        return [
+          "http://img1.qunarzz.com/sight/p0/1810/cc/cc47164357acbeb5a3.water.jpg_r_800x800_0271f7e2.jpg",
+          "http://img1.qunarzz.com/sight/p0/1810/32/32392eea822d11c8a3.water.jpg_r_800x800_4172a5f9.jpg"
+        ];
+      }
+    }
+  },
+  data() {
+    return {
+      swiperOptions: {
+        pagination: ".swiper-pagination",
+        paginationType: "fraction",
+        observer: true,
+        observeParents: true
+      }
+    };
+  },
+  methods: {
+    handleGallaryClick() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+.container >>> .swiper-container
+  overflow: inherit
+.container
+  display: flex
+  flex-direction: column
+  justify-content: center
+  z-index: 99
+  position: fixed
+  left: 0
+  right: 0
+  top: 0
+  bottom: 0
+  background: #000
+  .wrapper
+    height: 0
+    width: 100%
+    padding-bottom: 67%
+    .gallary-img
+      width: 100%
+    .swiper-pagination
+      color: #fff
+      bottom: -1rem
+</style>
---
+++ src/pages/detail/components/DetailBanner.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div
+      class="banner"
+      @click="handleBannerClick"
+    >
+      <img
+        class="banner-img"
+        src="http://img1.qunarzz.com/sight/p0/1810/cc/cc47164357acbeb5a3.water.jpg_600x330_aed5d0a5.jpg"
+      />
+      <div class="banner-info">
+        <div class="banner-title">
+          海洋乐园
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe755;</span>
+          25
+        </div>
+      </div>
+    </div>
+    <detail-gallary
+      :imgs="imgs"
+      @close="closeGallary"
+      v-show="showGallary"
+    ></detail-gallary>
+  </div>
+</template>
+
+<script>
+import DetailGallary from "common/gallary/Gallary";
+export default {
+  data() {
+    return {
+      imgs: [
+        "http://img1.qunarzz.com/sight/p0/1810/cc/cc47164357acbeb5a3.water.jpg_r_800x800_0271f7e2.jpg",
+        "http://img1.qunarzz.com/sight/p0/1810/32/32392eea822d11c8a3.water.jpg_r_800x800_4172a5f9.jpg",
+        "http://img1.qunarzz.com/sight/p0/1810/54/54a97e731330c416a3.water.jpg_r_800x800_3a69f2a8.jpg"
+      ],
+      showGallary: false
+    };
+  },
+  components: {
+    DetailGallary
+  },
+  methods: {
+    handleBannerClick() {
+      this.showGallary = true;
+    },
+    closeGallary() {
+      this.showGallary = false;
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.banner
+  position: relative
+  overflow: hidden
+  height: 0
+  padding-bottom: 55%
+  .banner-img
+    width: 100%
+  .banner-info
+    display: flex
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    line-height: 0.6rem
+    color: #fff
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+    .banner-title
+      flex: 1
+      font-size: 0.32rem
+      padding: 0 0.2rem
+    .banner-number
+      height: 0.32rem
+      line-height: 0.32rem
+      margin-top: 0.14rem
+      padding: 0 0.4rem
+      border-radius: 0.2rem
+      background: rgba(0, 0, 0, 0.8)
+      font-size: 0.24rem
+      .banner-icon
+        font-size: 0.24rem
+</style>
